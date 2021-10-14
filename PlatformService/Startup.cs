1ebfac0c5a259b6e36602247ed9d2df33bd79bb9
petr.sokolnikov <petr.sokolnikov@bookedby.com>
2021-10-14 03:16:58 +0300

    PART 6 - MULTI-RESOURCE API
    - End Point Review for Commands Service
    - Data Layer - Models
    - Data Layer - DB Context
    - Data Layer - Repository
    - Data Layer - Dtos
    - Data Layer - AutoMapper Profiles
    - Controller & Actions

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
@@ -29,7 +23,6 @@ namespace PlatformService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             if (_env.IsProduction())
@@ -55,7 +48,6 @@ namespace PlatformService
             Console.WriteLine($"--> CommandService Endpoint {Configuration["CommandService"]}");
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
